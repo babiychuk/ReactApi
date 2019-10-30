dce27a621267850de670cb2842ba587f83c2bed3
Евгений Бабийчук <e.babiychuk@jr.local>
2019-10-30 16:44:46 +0200

    Updates

---
+++ precache-manifest.0ed469a65ef78622a0fa9a624d67d431.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c60cdfb3c60f7ef2b2ce",
+    "url": "/react-landing-page-creator/static/js/runtime~main.dc5aec13.js"
+  },
+  {
+    "revision": "b4ac7be4925712971a5a",
+    "url": "/react-landing-page-creator/static/js/main.590dc42b.chunk.js"
+  },
+  {
+    "revision": "ae0b72b89335021e0997",
+    "url": "/react-landing-page-creator/static/js/2.4952e05c.chunk.js"
+  },
+  {
+    "revision": "b4ac7be4925712971a5a",
+    "url": "/react-landing-page-creator/static/css/main.5a1d5224.chunk.css"
+  },
+  {
+    "revision": "028075e2738527b3a53c5accc7babec8",
+    "url": "/react-landing-page-creator/index.html"
+  }
+];
